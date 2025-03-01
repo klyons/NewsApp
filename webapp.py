527c71a8a692ac51bcd5fea59bf1ae4521f17283
siri2009k <siri.kelshikar@gmail.com>
2025-02-28 17:36:22 -0800

    word cloud

--- webapp.py
+++ webapp.py
@@ -1,5 +1,17 @@
 #https://blog.streamlit.io/langchain-streamlit/
 
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from wordcloud import WordCloud
 
-st.write("hi")
+
+
+st.markdown("<style>h1{text-align: center;}</style>", unsafe_allow_html=True)
+st.title("Welcome to _:violet[Newsapp]_!")
+
+
+def wordCloud(name):
+	df = pd.read_csv(name) #name needs to be a string
+	#text = " ".join(title for title in df.Title)
+	df.head()
