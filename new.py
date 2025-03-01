99107037089a623c9d13bb3a12009f9b6edbd66d
siri2009k <siri.kelshikar@gmail.com>
2025-02-28 18:12:00 -0800

    all new updates

--- new.py
+++ new.py
@@ -1,18 +1,10 @@
-#https://blog.streamlit.io/langchain-streamlit/
-
-import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud, STOPWORDS
 
 
-
-st.markdown("<style>h1{text-align: center;}</style>", unsafe_allow_html=True)
-st.title("Welcome to _:violet[Newsapp]_!")
-
-
-
 def read_csv_file(file_path):
+    print("first function")
     """
     Reads a CSV file and returns its content as a list of dictionaries.
     Each dictionary represents a row, with keys being the header names.
