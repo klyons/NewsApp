afb46747b7772675e0435c5cd67a9edc3d8d5ed4
siri2009k <siri.kelshikar@gmail.com>
2025-03-14 18:16:42 -0700

    sentiment finished

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,1 +1,32 @@
 # measuring sentiment of a text
+
+from bs4 import BeautifulSoup
+from textblob import TextBlob
+import requests
+
+# textblob -> NLP library/module
+# requests/bs4
+
+# 1. User input
+userLink = input("Enter a link to a review: ")
+
+response = requests.get(userLink)
+
+# 2. Fetch then parse data
+if response.status_code == 200:
+  soup = BeautifulSoup(response.content, 'html.parser')
+  text = soup.get_text()
+
+  blob = TextBlob(text)
+  print(type(blob))
+  #blob_class = 
+
+  # polarity: -1 to 1
+  # subjectivity: 0 to 1
+
+  # Analyze sentiment
+  sentiment = blob.sentiment
+  print(sentiment)
+
+else:
+  print("cannot fetch")
