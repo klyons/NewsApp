60f51d027ed827734a6a4d9e64b21d5dee548bf5
siri2009k <siri.kelshikar@gmail.com>
2025-05-23 17:38:23 -0700

    fixed bbc

--- parser.py
+++ parser.py
@@ -35,40 +35,12 @@ class Parser():
 		else:
 			print(f"Failed to fetch {address}, status code: {response.status_code}")
 		return pd.DataFrame(hrefs, columns=["hrefs"])
-	
-	def parse_bbc(self, df):
-		#find the header and the subheader
-		df = self.create_columns(df)
-		# i want the item in the columns []
-		for i, link in enumerate(df.iloc[0]):
-			response = requests.get(link)
-			if response.status_code == 200:
-				soup = BeautifulSoup(response.content, 'html.parser')
-				
-				# Extract header
-				header = soup.find('h1')
-				if header:
-					if self.print:
-						print(header.get_text(strip=True))
-					df.loc[i, "header"] = header.get_text(strip=True)
-				
-				# Extract tagline
-				tagline = soup.find_all('p')
-				if tagline:
-					if self.print:
-						print(tagline[0].get_text(strip=True))
-					df.loc[i, "tagline"] = tagline.get('id')
-
-			else:
-				print(f"Failed to fetch {link}, status code: {response.status_code}")
 						
 	def parse_motherjones(self, df):
 		df = self.create_columns(df)
-		pdb.set_trace()
 		# i want the item in the columns []
 		for i, link in enumerate(df.iloc[0]):
 			response = requests.get(link)
-			pdb.set_trace()
 			if response.status_code == 200:
 				soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -76,17 +48,15 @@ class Parser():
 				header = soup.find_all(class_= 'entry-text')
 				if header:
 					if self.print:
-						pdb.set_trace()
 						#print(header.get_text(strip=True))
-					df.loc[i, "header"] = header.get_text(strip=True)
+						df.loc[i, "header"] = header.get_text(strip=True)
 				
 				#find tagline
 				tagline = soup.find('h2')
 				if tagline:
 					if self.print:
-						pdb.set_trace()
 						#print(tagline.get_text(strip=True))
-					df.loc[i, "tagline"] = tagline.get_text(strip=True)
+						df.loc[i, "tagline"] = tagline.get_text(strip=True)
 
 				#find date
 				date = soup.find(class_ = "dateline")
@@ -105,7 +75,7 @@ class Parser():
 				soup = BeautifulSoup(response.content, 'html.parser')
 
 				#find header
-				header = soup.find('h1')
+				header = soup.find(class_ = "sc-f98b1ad2-0 dfvxux")
 				if header:
 					df.loc[i, "header"] = header.get_text(strip=True)
 				
@@ -114,7 +84,7 @@ class Parser():
 				if tagline[0]: 
 					df.loc[i, "header"] = tagline[0].get_text(strip=True)
 
-				date = soup.find(class_ = "sc-2b5e3b35-2 fkLXLN")
+				date = soup.find()
 				if date:
 					df.iloc[i, 'date'] = date.get_text()
 
@@ -178,15 +148,15 @@ class Parser():
 			if response.status_code == 200:
 				soup = BeautifulSoup(response.content, 'html.parser')
 				#find header
-				header = soup.find("h1")
+				header = soup.find(class_= "headline speakable")
 				if header: 
 					df.loc[i, "header"] = header.get_text(strip=True)
 				#find tagline
-				tagline = soup.find("h2")
+				tagline = soup.find(class_ = "sub-headline speakable")
 				if tagline:
 					df.loc[i, "tagline"] = tagline.get_text(strip=True)
 
-				date = soup.find(class_ = "article-date")
+				date = soup.find(class_ = "time")
 				if date:
 					df.iloc[i, 'date'] = date.get_text()
 
