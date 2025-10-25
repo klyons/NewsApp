228e5f7cdd8c65f42fc4b6994b5ceb85db42e545
siri2009k <siri.kelshikar@gmail.com>
2025-10-24 17:28:40 -0700

    rss parser

--- lib/rssparser.py
+++ lib/rssparser.py
@@ -1,11 +1,14 @@
 feeds = {
-"bbc": "",
+"bbc": "https://feeds.bbci.co.uk/news/politics/rss.xml",
 "cnn": "http://rss.cnn.com/rss/cnn_allpolitics.rss",
 "fox": "https://moxie.foxnews.com/google-publisher/politics.xml",
 "jpost": "https://www.jpost.com/rss/rssfeedsamerican-politics",
-"newsmax": "",
+"newsmax": "https://www.newsmax.com/rss/Politics/1/",
 "motherjones": "",
 "aljazeera": "",
 "ap": "",
-"msnbc": ""
+"msnbc": "https://feeds.nbcnews.com/msnbc/public/news"
 }
+
+#https://about.fb.com/wp-content/uploads/2016/05/rss-urls-1.pdf
+#https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml
